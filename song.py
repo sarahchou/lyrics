647d3264458a9b4306e3e7541c94a3ebf928accb
sarahchou <chou.s@husky.neu.edu>
2020-09-23 16:00:00 -0700

    addalbum fixing bugs adding songs to init

--- song.py
+++ song.py
@@ -2,6 +2,9 @@ import unittest, random
 class Discography:
     def __init__(self, albums):
         self.albums = albums
+    def addAlbum(self, a):
+        self.albums.append(a)
+
     def numSongs(self):
         c = 0
         for a in self.albums:
@@ -13,6 +16,7 @@ class Discography:
             for s in a.getSongs():
                 arr.append(s)
         return arr
+
     def getSongsInAlbum(self, album):
         album_songs = []
         for a in self.albums:
@@ -30,7 +34,7 @@ class Discography:
         num = 0
         for s in self.getSongs():
             num += s.occursInLyrics(word)
-        print(word + " occurs in discography " + str(num) + " times")
+        print("The word " + word.lower() + " occurs in discography a total of " + str(num) + " times")
         return num
 
     def randomLyric(self):
@@ -61,12 +65,12 @@ class Album:
         print(word + " occurs in " + self.title + " " + str(num) + " times")
         return num
     def addSongToAlbum(self, song):
-        if song.getAlbum() == self.title and song not in self.songs:
+        if song.getAlbumTitle() == self.title and song not in self.songs:
             self.songs.append(song)
 
 class Song:
     
-    def __init__(self, title="", album="", track_num=0, lyric_file = ""):
+    def __init__(self, title="", album=None, track_num=0, lyric_file = ""):
         self.title = title
         self.album = album
         self.track_num = track_num
@@ -76,20 +80,23 @@ class Song:
         return self.title
     def getAlbum(self):
         return self.album
+    def getAlbumTitle(self):
+        return self.album.getTitle()
     def getTrackNum(self):
         return self.track_num
     def getLyrics(self):
         return readFile(self.lyric_file)
     def getLyricFile(self):
         return self.lyric_file
-
     def occursInLyrics(self, word):
         counter = 0
         for w in self.getLyrics():
             if word.lower() == w.lower():
                 counter += 1
-        print(word + " occurs in " + self.getTitle() + " " + str(counter) + " times.")
+        if counter > 0:
+            print(word + " occurs in " + self.getTitle() + " " + str(counter) + " times.")
         return counter
+
 def readFile(fileName):
     fileObj = open(fileName, "r")
     lyrics = fileObj.read().split()
@@ -97,29 +104,56 @@ def readFile(fileName):
     return lyrics
 
 def init_songs():
-    wonderland = Song("Wonderland", "1989", 5, "wonderland_lyrics.txt")
-    new_romantics = Song("New Romantics", "1989", 2, "new_romantics_lyrics.txt")
-    wildest_dreams = Song("Wildest Dreams", "1989", 1, "wildest_dreams_lyrics.txt")
-    red = Song("Red", "Red", 1, "red_lyrics.txt")
-
-
+    album_taylorswift = Album("Taylor Swift", [])
+    album_fearless = Album("Fearless", [])
+    album_speaknow = Album("Speak Now", [])
     album_red = Album("Red", [])
     album_1989 = Album("1989", [])
+    album_reputation = Album("reputation", [])
+    album_lover = Album("Lover", [])
+    album_folklore = Album("folklore", [])
+    # debut songs
+    tim_mcgraw = Song("Tim McGraw", album_taylorswift, 1, "discography/taylorswift/tim_mcgraw_lyrics.txt")
+    picture_to_burn = Song("Picture to Burn", album_taylorswift, 2, "discography/taylorswift/picture_to_burn_lyrics.txt")
+    teardrops_on_my_guitar = Song("Teardrops on my Guitar", album_taylorswift, 3, "discography/taylorswift/teardrops_on_my_guitar_lyrics.txt")
+    a_place_in_this_world = Song("A Place in this World", album_taylorswift, 4, "discography/taylorswift/a_place_in_this_world_lyrics.txt")
+    cold_as_you = Song("Cold as You", album_taylorswift, 5, "discography/taylorswift/cold_as_you_lyrics.txt")
+    the_outside = Song("The Outside", album_taylorswift, 6, "discography/taylorswift/the_outside_lyrics.txt")
+    tied_together_with_a_smile = Song("Tied Together with a Smile", album_taylorswift, 7, "discography/taylorswift/tied_together_with_a_smile_lyrics.txt")
+    stay_beautiful = Song("Stay Beautiful", album_taylorswift, 8, "discography/taylorswift/stay_beautiful_lyrics.txt")
+    shouldve_said_no = Song("Should've Said No", album_taylorswift, 9, "discography/taylorswift/shouldve_said_no_lyrics.txt")
+    marys_song = Song("Mary's Song (Oh My My My)", album_taylorswift, 10, "discography/taylorswift/marys_song_lyrics.txt")
+    our_song = Song("Our Song", album_taylorswift, 11, "discography/taylorswift/our_song_lyrics.txt")
+    im_only_me_when_im_with_you = Song("I'm Only Me When I'm With You", album_taylorswift, 12, "discography/taylorswift/im_only_me_when_im_with_you_lyrics.txt")
+    invisible = Song("Invisible", album_taylorswift, 13, "discography/taylorswift/invisible_lyrics.txt")
+    a_perfectly_good_heart = Song("A Perfectly Good Heart", album_taylorswift, 14, "discography/taylorswift/a_perfectly_good_heart_lyrics.txt")
 
 
-    listofsongs = [wonderland, wildest_dreams, new_romantics, red]
-    listofalbums = [album_1989, album_red]
+    wonderland = Song("Wonderland", album_1989, 5, "discography/1989/wonderland_lyrics.txt")
+    new_romantics = Song("New Romantics", album_1989, 2, "discography/1989/new_romantics_lyrics.txt")
+    wildest_dreams = Song("Wildest Dreams", album_1989, 1, "discography/1989/wildest_dreams_lyrics.txt")
+    red = Song("Red", album_red, 1, "discography/red/red_lyrics.txt")
+
+
+    listofsongs = [wonderland, wildest_dreams, new_romantics, red, tim_mcgraw, picture_to_burn, teardrops_on_my_guitar,
+    a_place_in_this_world, cold_as_you, the_outside, tied_together_with_a_smile, stay_beautiful, shouldve_said_no,
+    marys_song, our_song, im_only_me_when_im_with_you, invisible, a_perfectly_good_heart]
+    listofalbums = [album_taylorswift, album_fearless, album_speaknow, album_red, album_1989, album_reputation,
+    album_lover, album_folklore]
     for s in listofsongs:
         for a in listofalbums:
-            a.addSongToAlbum(s)
-    disc = Discography([album_1989, album_red])
+            if s.getAlbum() == a:
+                a.addSongToAlbum(s)
+    disc = Discography([])
+    for a in listofalbums: 
+        disc.addAlbum(a)
     return disc
 
 
 if __name__ == "__main__":
     disc = init_songs()
     print(disc.randomLyric())
-    words = ["my", "and", "wonderland", "he", "wOndErlaNd", "rEd", "RED"]
+    words = ["my", "and", "wonderland", "he", "wOndErlaNd", "rEd", "RED", "heart", "cold"]
     print(disc.occursInDiscography(random.choice(words)))
     print(str(disc.numSongs()), "num songs")
 
